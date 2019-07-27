8a7ad672531425fbd19a413d5ce3dc983360ddc9
jonghui7179 <53362426+jonghui7179@users.noreply.github.com>
2019-07-27 20:27:23 +0900

    chapter1
    화면에 문자 표시하기

---
+++ Hello1B.java
@@ -0,0 +1,8 @@
+/*화면에 연속 표시 방법2 */
+
+public class Hello1B {
+    public static void main(String[] args){
+        System.out.print("첫 java 프로그램입니다.");
+        System.out.print("화면에 출력하고 있습니다.");
+    }
+}
