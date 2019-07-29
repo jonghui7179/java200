ba4504f9b089e10fbb1b1d6af217c0c2afef9655
Jonghui-Park <dawnofdev@gmail.com>
2019-07-29 21:30:11 +0900

    upload

---
+++ IdeaProjects/java200/chapter2/SumAve1.java
@@ -0,0 +1,16 @@
+class SumAve1{
+    public static void main(String[] args) {
+
+        int x;
+        int y;
+
+        x=63;
+        y=18;
+
+        System.out.println("x값은"+x+"입니다.");
+        System.out.println("y값은"+y+"입니다.");
+        System.out.println("합계는"+(x+y)+"입니다.");
+        System.out.println("퍙균은"+(x+y)/2+"입니다.");
+        //정수/정수는 소수점을 버리고 계산되서 40.5가 아니라 40이다.
+    }
+}
---
+++ IdeaProjects/java200/chapter2/SumAve3C.java
@@ -0,0 +1,13 @@
+//3개의 변수의 합과 평균을 표시하는 방법 . 초기화와 대입
+
+class SumAve3C {
+    public static void main(String[] args) {
+        int x=63;
+        int y=18;
+        int z=52;
+        int sum=x+y+z;
+
+        System.out.println("합계는"+sum+"입니다.");
+        System.out.println("평균은 "+sum/3+"입니다.");
+    }
+}
