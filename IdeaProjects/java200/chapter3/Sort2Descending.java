92aa45894cdf16ea6ecd9ae33ad13bde7e7dfde5
Jonghui-Park <dawnofdev@gmail.com>
2019-07-30 19:42:13 +0900

    upload

---
+++ IdeaProjects/java200/chapter3/Sort2Descending.java
@@ -0,0 +1,22 @@
+//두 정숫값을 읽어서 내림차순으로 정렬
+
+import java.util.Scanner;
+
+class Sort2Descending {
+    public static void main(String[] args) {
+        Scanner stdIn = new Scanner(System.in);
+
+        System.out.print("정수 a:"); int a = stdIn.nextInt();
+        System.out.print("정수 b:"); int b = stdIn.nextInt();
+
+        if (a<b) {
+            int t = a;
+            a = b;
+            b = t;
+        }
+
+        System.out.println("a>=b가 되도록 정렬했습니다.");
+        System.out.println("변수 a는 "+a+"입니다.");
+        System.out.println("변수 b는 "+b+"입니다.");
+    }
+}
